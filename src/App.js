dfdc097f7fdab23a2102ec1237a94eccd946c9f4
nandhagopal <blackrossen11@gmail.com>
2021-04-03 16:12:59 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from "react";
-import { BrowserRouter, Route, Switch, Redirect, useHistory, useLocation, } from "react-router-dom";
-import { createBrowserHistory } from "history";
+import { BrowserRouter, Route, Switch} from "react-router-dom";
 import './App.css';
 import Header from './Components/Header';
-import Home from './Pages/Home/Home';
-import Calculator from './Pages/Calculator';
-import Footer from "./Components/Footer";
 import TicketGen from "./Pages/Technology/TicketGen";
 function App() {
 
@@ -15,17 +11,11 @@ function App() {
         {/* Common Pages for All roles */}
         <Header />
         <Switch>
-          <Route exact path="/" render={props => <Home {...props}></Home>} />
-          <Route exact path="/calculator" render={props => <Calculator {...props} />} />
-          <Route exact path="/ins-trans-ticket-generation" render={props => <TicketGen />} />
+          <Route path="/" render={props => <TicketGen {...props}></TicketGen>} />
         </Switch>
         {/* <Footer /> */}
       </BrowserRouter>
     </div>
   );
-
-
-
 }
-
 export default App;
